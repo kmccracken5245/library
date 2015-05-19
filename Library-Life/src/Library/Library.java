f9468f13b8ea6d7d5b2ebdf20238ba85a5ed49fb
kmccracken5245 <kmccracken5245@001LAB102010540.neisd.ad>
2015-05-19 15:58:00 -0500

    added more stuff

--- Library-Life/src/Library/Library.java
+++ Library-Life/src/Library/Library.java
@@ -1,6 +1,13 @@
 package Library;
 
+import
 public class Library {
-	static int
+	static int maxCustomers = 100;
+	static int maxBooks = 100; 
+	static customers[] customer = new customers[maxCustomers];
+	static  
+	
+	
+	
 
 }
